fd1d754b0e2882f14543bcf78dc31f82ad61c26e
Miryam-Choor <M400miryam@gmail.com>
2026-08-30 13:43:10 +0300

    fix errorHandler, change the link in register.js

--- public/js/regiter.js
+++ public/js/regiter.js
@@ -11,14 +11,13 @@ document.getElementById('registerForm').addEventListener('submit', async (e) => 
     messageDiv.textContent = '';
 
     try {
-        const response = await fetch('/api/register', {
+        const response = await fetch('http://localhost:3000/api/register', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({ username, email, password })
         });
-
         const data = await response.json();
 
         if (!response.ok) {
