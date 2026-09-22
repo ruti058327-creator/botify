6ab8e69538b6cf6eebdddbb0cb42259ce7e54109
noa <noar57863@gmail.com>
2026-09-22 18:23:04 +0300

    Add newline at the end of server.js for consistency

--- server/server.js
+++ server/server.js
@@ -42,6 +42,7 @@ app.use((err, req, res, next) => {
 
 // 6. הפעלת השרת
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on http://localhost:${PORT}`);
 });
